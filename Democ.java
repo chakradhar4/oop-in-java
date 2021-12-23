24550e0e198f8d4c4f9bfaf3f195520425bb425e
chakradhar4 <chakradharghute@gmail.com>
2021-12-23 22:09:34 +0530

    first commit

---
+++ Democ.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Democ{
+	static Scanner ax;
+	public static void main(String args[])
+	{
+		 ax = new Scanner(System.in);
+
+		// Use of nextLine() method
+		System.out.println("enter : ");
+		String Inpt = ax.next();
+		System.out.println(Inpt);
+	}
+}
+
+    
+
